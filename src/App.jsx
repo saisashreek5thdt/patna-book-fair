e91bd1d8beefef79dc9f8d2cbba10131bb320b16
Rakeshvanam6868 <https://github.com/Rakeshvanam6868>
2025-02-04 13:58:04 +0530

    Cleaned the structure and files

--- src/App.jsx
+++ src/App.jsx
@@ -5,55 +5,7 @@ import Home from "./Home/Home";
 function App() {
   useEffect(() => {
     // List of scripts to include
-    const scripts = [
-      "assets/vendor/swiper/swiper-bundle.min.js",
-      "assets/vendor/splide/splide.min.js",
-      "assets/vendor/slim-select/slimselect.min.js",
-      "assets/vendor/fslightbox/fslightbox.js",
-      "assets/vendor/splide/splide-extension-auto-scroll.min.js",
-      "assets/vendor/lenis/lenis.min.js",
-      "assets/vendor/splittype/index.min.js",
-      "assets/vendor/gsap/gsap.min.js",
-      "assets/vendor/gsap/gsap-scroll-trigger.min.js",
-      "assets/js/mainEvent.js",
-      "assets/js/countdown.js",
-    ];
-
-    // Function to dynamically load a script
-    const loadScript = (src) => {
-      return new Promise((resolve, reject) => {
-        const script = document.createElement("script");
-        script.src = src;
-        script.async = true;
-        script.onload = () => resolve();
-        script.onerror = () =>
-          reject(new Error(`Failed to load script: ${src}`));
-        document.body.appendChild(script);
-      });
-    };
-
-    // Load all scripts sequentially
-    const loadScripts = async () => {
-      try {
-        for (const src of scripts) {
-          await loadScript(src);
-        }
-      } catch (error) {
-        console.error(error);
-      }
-    };
-
-    loadScripts();
-
-    // Cleanup function
-    return () => {
-      scripts.forEach((src) => {
-        const script = document.querySelector(`script[src="${src}"]`);
-        if (script) {
-          document.body.removeChild(script);
-        }
-      });
-    };
+   
   }, []);
 
   const router = createBrowserRouter([
--- src/Components/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Sidebar } from "./Home/Sidebar";
-export { default as Header } from "./Home/Header";
-export { default as Banner } from "./Main/Banner";
-export { default as AboutSection } from "./Main/AboutSection";
